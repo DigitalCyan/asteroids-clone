4264038d65640e9882f40a3ef27c3c27ee613081
DigitalCyan <marinoljubetic8@gmail.com>
2020-08-30 03:10:37 +0200

    Added GameController.cs

---
+++ Assets/Sprites/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    static public GameController instance {
+        get {
+            if (!_instance) {
+                GameObject obj = new GameObject("GameController");
+                _instance = obj.AddComponent<GameController>();
+            }
+            return _instance;
+        }
+    }
+    static private GameController _instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
